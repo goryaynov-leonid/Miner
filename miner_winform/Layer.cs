0cb08e2270a84ad64052c4834b388a49717f6685
Leonid Goryaynov <lbglbg999@gmail.com>
2020-01-11 13:52:32 +0700

    Refactor Primitive Obsession

---
+++ miner_winform/Layer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace miner_winform
+{
+    public class Layer
+    {
+        public int ZCoordinate { get; set; }
+        
+        public Layer(int zCoordinate)
+        {
+            ZCoordinate = zCoordinate;
+        }
+
+    }
+}
